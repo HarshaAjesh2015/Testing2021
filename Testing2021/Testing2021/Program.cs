b8e99ca3e5e754ef642795d300041b011d5dda52
unknown <harshagopinathan@gmail.com>
2021-12-20 13:15:33 +1000

    first commit

---
+++ Testing2021/Testing2021/Program.cs
@@ -0,0 +1,133 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace Testing2021
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+           
+
+            IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
+            usernameTextbox.SendKeys("hari");
+
+            IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
+            passwordTextbox.SendKeys("123123");
+
+            IWebElement loginButton = driver.FindElement(By.XPath("//*[@id='loginForm']/form/div[3]/input[1]"));
+            loginButton.Click();
+
+            
+
+
+            IWebElement hellohari = driver.FindElement(By.XPath("//*[@id='logoutForm']/ul/li/a"));
+
+            if (hellohari.Text == "Hello hari!")
+
+            {
+                Console.WriteLine("Logged in successfully, test positive");
+            }
+            else
+            {
+                Console.WriteLine("Login failed, test negative");
+
+            }
+
+            IWebElement administrationDropdown = driver.FindElement(By.XPath("/ html / body / div[3] / div / div / ul / li[5] / a"));
+            administrationDropdown.Click();
+
+           
+            IWebElement tmOption = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
+            tmOption.Click();
+
+            IWebElement createNewButton = driver.FindElement(By.XPath("//*[@id='container']/p/a"));
+            createNewButton.Click();
+           
+            IWebElement typeCodeDropdown = driver.FindElement(By.XPath("//*[@id='TimeMaterialEditForm']/div/div[1]/div/span[1]/span/span[1]"));
+            typeCodeDropdown.Click();
+
+            IWebElement timeOption = driver.FindElement(By.XPath("//*[@id='TimeMaterialEditForm']/div/div[1]/div/span[1]/span/span[1]"));
+            timeOption.Click();
+
+            IWebElement codeTextbox = driver.FindElement(By.Id("Code"));
+            codeTextbox.SendKeys("Testing2021");
+
+            IWebElement descriptionTextbox = driver.FindElement(By.Id("Description"));
+            descriptionTextbox.SendKeys("New Entry Testing2021");
+
+
+            IWebElement priceTag = driver.FindElement(By.XPath("//*[@id='TimeMaterialEditForm']/div/div[4]/div/span[1]/span/input[1]"));
+            priceTag.Click();
+
+            IWebElement priceTextbox = driver.FindElement(By.Id("Price"));
+            priceTextbox.SendKeys("20");
+
+            IWebElement saveButton = driver.FindElement(By.Id("SaveButton"));
+            saveButton.Click();
+            Thread.Sleep(2000);
+
+            IWebElement goToLastPageButton = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[4]/a[4]/span"));
+            goToLastPageButton.Click();
+            Thread.Sleep(2000);
+
+            IWebElement actualCode = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[last()]/td[1]"));
+
+            if (actualCode.Text == "Testing2021")
+            {
+                Console.WriteLine("Time record created successfully, test passed.");
+            }
+            else
+            {
+                Console.WriteLine("Creating record failed,test failed.");
+            }
+
+             administrationDropdown = driver.FindElement(By.XPath("/ html / body / div[3] / div / div / ul / li[5] / a"));
+            administrationDropdown.Click();
+
+
+            tmOption = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
+            tmOption.Click();
+
+            goToLastPageButton = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[4]/a[4]/span"));
+            goToLastPageButton.Click();
+            Thread.Sleep(2000);
+
+            IWebElement editButton = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[last()]/td[5]/a[1]"));
+            editButton.Click();
+
+            IWebElement descriptionEdit = driver.FindElement(By.Id("Description"));
+            descriptionEdit.SendKeys("Edit New Entry Testing2021");
+
+            IWebElement saveEditbutton = driver.FindElement(By.Id("SaveButton"));
+            saveEditbutton.Click();
+            Thread.Sleep(2000);
+
+            IWebElement editCode = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[last()]/td[5]/a[1]"));
+          
+            if (editCode.Text == "Edit New Entry Testing2021")
+            {
+                Console.WriteLine("Edit record created successfully, test passed.");
+            }
+            else
+            {
+                Console.WriteLine("Editing record failed,test failed.");
+            }
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
